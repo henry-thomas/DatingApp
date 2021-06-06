54747e4a397fea23bf5f5f9f9a2b830a50046c47
Henry Thomas <henrythomastzn@live.com>
2021-06-06 20:55:18 +0200

    First commit

---
+++ API/Controllers/-- SQLite.sql
@@ -0,0 +1,9 @@
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (1, "Wynand");
+
+INSERT INTO Users (Id, UserName)
+VALUES (2, "Petrone");
+
+INSERT INTO Users (Id, UserName)
+VALUES (3, "Henry");
