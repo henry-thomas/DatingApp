738feb4e3ff225e1a29e544942a7faa7f3522209
Henry Thomas <henrythomastzn@live.com>
2021-07-15 07:31:07 +0200

    End of section 9

---
+++ client/src/app/_services/members.service.ts
@@ -0,0 +1,24 @@
+import { Member } from '../_models/member';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MembersService {
+  baseUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  getMembers() {
+    return this.http.get<Member[]>(this.baseUrl + 'users');
+  }
+  
+  getMember(username: string) {
+    return this.http.get<Member>(this.baseUrl + 'users/' + username);
+  }
+}
